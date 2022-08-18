61bf9376668925bbed93b7e754eaf5ee2d3018ea
Bayramzade066 <bayramzade.313@gmail.com>
2022-08-18 04:17:49 +0400

    design edit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div >
       <ScrollToTop showUnder={1500} easing={'linear'} duration={800} style={{zIndex: 9999,right: 50, bottom: 50}}>
-        <div className="absolute z-[9999] rounded-full hidden sm:flex items-center justify-center p-1 min-w-[24.5px] sm:min-w-[45px] bg-black hover:bg-[#ff5413] hover:text-black transition-all hover:transitin-all text-primary text-xl sm:text-3xl">&#8682;</div>
+        <div className="absolute z-[9999] rounded-full hidden sm:flex items-center justify-center p-1 min-w-[20px] sm:min-w-[40px] -rotate-90  text-primary text-xl sm:text-3xl"><img src="/next.png" className="w-full h-full" alt="" /></div>
       </ScrollToTop>
       <div className="bg-[url('../public/header_background.png')] bg-local bg-cover sm:bg-auto 2xl:bg-cover bg-right-top bg-no-repeat ">
         <Navbar />
