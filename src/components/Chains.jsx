7429852bc418405bf95496825385c911e14e53df
rijatalizada <rijatga@code.edu.az>
2022-11-10 10:59:49 +0400

    WebsiteRemastered

--- src/components/Chains.jsx
+++ src/components/Chains.jsx
@@ -5,17 +5,39 @@ const Chains = () => {
     <div className='bg-white py-10 mt-10 md:mt-0 flex flex-col items-center justify-center'>
         <h1 className='text-2xl font-semibold'>Live <span className='text-[#FF7348]'>on</span></h1>
         <div className="chains flex w-[60%] justify-between mt-11">
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Ethereum.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Solana.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Celo.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Avax.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Polygon.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Binance.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Gnosis.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Optimism.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Arbitrium.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Fantom.svg" alt=""/>
-            <img className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" src="Icons/Chains/Aurora.svg" alt=""/>
+            <a href="https://ethereum.org/en/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Ethereum.svg" alt=""/>
+            </a>
+            <a href="https://solana.com/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Solana.svg" alt=""/>
+            </a>
+            <a href="https://celo.org/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Celo.svg" alt=""/>
+            </a>
+            <a href="https://www.avax.network/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Avax.svg" alt=""/>
+            </a>
+            <a href="https://polygon.technology/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Polygon.svg" alt=""/>
+            </a>
+            <a href="https://www.bnbchain.org/en" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Binance.svg" alt=""/>
+            </a>
+            <a href="https://gnosischain.com/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Gnosis.svg" alt=""/>
+            </a>
+            <a href="https://www.optimism.io/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Optimism.svg" alt=""/>
+            </a>
+            <a href="https://arbitrum.io/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Arbitrium.svg" alt=""/>
+            </a>
+            <a href="https://fantom.foundation/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Fantom.svg" alt=""/>
+            </a>
+            <a href="https://aurora.dev/" target="_blank">
+              <img className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 cursor-pointer" src="Icons/Chains/Aurora.svg" alt=""/>
+            </a>
         </div>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,32 +5,33 @@ export default function Header() {
     <div className="w-full h-full">
       <div className="text-center flex flex-col items-center px-8  space-y-12 mt-24 tracking-wider justify-center w-full">
         <h1 className="text-[#000000] font-bold text-[3.62rem] w-full ">
-            Simplified and Collaborative
+          Simplified and Collaborative
           <span className="block">
             Treasury Management for <span className="text-primary">DAOs</span>
           </span>
         </h1>
         <h2 className="text-[#707070] px-12  sm:px-1 text-center tracking-wider break-words !mt-[2rem] font-medium text-base">
-          Connect your organization wallets, onboard your contributors, automate
-          your payroll, <br className="hidden sm:block" />
-          manage your organization expenses, do budgeting & reporting and make
-          investments <br className="hidden sm:block" />
-          in DeFi while mitigating risks with dynamic risk assessments.
+          Connect your organization wallets, onboard contributors, run payroll, do budgeting& bookkeeping, 
+          <span className="block"> monitor your financial
+          data with live reporting and make investments in
+          </span>
+           <span className="block">DeFi while mitigating risks with
+          dynamic risk assessments.</span>
         </h2>
         <div className="grid grid-cols-2 gap-20 w-[72%] lg:w-[33%]  ">
           <button className="bg-[#FF7348] text-[#FFFFFF] rounded-md py-2 font-semibold text-lg">
-            <a href="">
-              Connect Wallet
-            </a>
+            <a href="">Connect Wallet</a>
           </button>
           <button className="text-[#FF7348] border-[1px] border-solid py-2 rounded-md border-[#FF7348] bg-transparent text-lg font-semibold">
-            <a href="">
-              View Demo
-            </a>
+            <a href="">View Demo</a>
           </button>
         </div>
         <div className="!mt-[7rem]">
-          <img src="Icons/Header/Dashboard_header.svg" alt="" className="w-[62rem] border-black border-solid border-4 rounded-md" />
+          <img
+            src="Icons/Header/Dashboard_header.svg"
+            alt=""
+            className="w-[62rem] border-black border-solid border-4 rounded-md"
+          />
         </div>
       </div>
     </div>
